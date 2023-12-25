b6a92dedee7e259445b1d7986a11346821df3442
isildonmez <isil.donmez@gmail.com>
2023-12-25 14:58:15 +1100

    Split code into a library crate

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::{fs, error::Error};
+
+pub struct Config {
+    pub keyword: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let keyword = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { keyword, file_path })
+    } 
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, fs, process, error::Error};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,36 +13,9 @@ fn main() {
     println!("Searching for {}", config.keyword);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process:: exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    keyword: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let keyword = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { keyword, file_path })
-    }
-    
 }
 
