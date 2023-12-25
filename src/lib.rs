336431ecc7913266f316c603be69f080f888ba20
isildonmez <isil.donmez@gmail.com>
2023-12-25 15:48:51 +1100

    Add  search function and tests for it

--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,38 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    
-    println!("With text:\n{contents}");
+
+    for line in search(&config.keyword, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(keyword: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(keyword) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let keyword = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        
+        assert_eq!(vec!["safe, fast, productive."], search(keyword, contents));
+    }
 }
