a110dc32879e2342702bfbe5c8cbd86a6c593d6e
isildonmez <isil.donmez@gmail.com>
2023-12-27 17:18:41 +1100

    Refactor using an iterator

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let keyword = args[1].clone();
-        let file_path = args[2].clone();
+        let keyword = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a keyword string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { keyword, file_path, ignore_case })
@@ -37,28 +45,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(keyword: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(keyword) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(keyword))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(keyword: &str, contents: &'a str) -> Vec<&'a str> {
     let keyword = keyword.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&keyword) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&keyword))
+        .collect()
 }
 
 #[cfg(test)]
